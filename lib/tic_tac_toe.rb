eaf9e25aa7e6a1bcc29473ede05446949576ce58
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-31 02:26:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,3 +42,13 @@ class TicTacToe
             return false
         end
     end
+
+    def turn_count
+        count = 0
+        @board.each do |element|
+            if element == "X" || element == "O"
+                count += 1
+            end
+        end
+        return count
+    end
