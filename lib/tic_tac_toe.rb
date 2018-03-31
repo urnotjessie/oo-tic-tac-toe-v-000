fba2c085d95bc541d66a561a1dd4d1456a0eb788
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-31 02:44:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,10 @@ class TicTacToe
     ]
 
     def display_board
-        @board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        
     end
 
     def input_to_index(user_input)
